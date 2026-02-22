5e4bceefdac81c560ae16b08dbc357764d9a0537
george1605 <32997165+george1605@users.noreply.github.com>
2026-02-22 14:49:49 +0200

    Added multiple spaces removal functionality

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ with open(sys.argv[1], "r+") as f:  # python3 main.py <file>
 	sep = ".,;:!?\'\""
 	for s in sep:
 		content = content.replace(s, "")
+	content = " ".join(s.split()) # split and join with 1 space (remove multiple spaces)
 	print("Content: ", content)
 	f.seek(0)
 	f.write(content)
-	f.truncate() 
+	f.truncate()
